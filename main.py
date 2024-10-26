e0489028f778b912d95f6a9b7081f212c73d9a58
SUGOOK <153060517+SUGOOK@users.noreply.github.com>
2024-10-26 21:24:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from mkvactrannumpy import calculate_value
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://sugook.github.io"],  # GitHub Pages 도메인 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
+
 
 # 입력값을 받기 위한 모델 정의
 class InputData(BaseModel):
